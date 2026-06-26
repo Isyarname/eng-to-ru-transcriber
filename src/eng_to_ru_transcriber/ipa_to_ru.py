52b83cc399dfbef0be23bd8b830fd75d0aa24dd3
Isyarname <alalala902@gmail.com>
2026-06-27 01:08:58 +0400

    Refactor: improve internal naming

--- src/eng_to_ru_transcriber/ipa_to_ru.py
+++ src/eng_to_ru_transcriber/ipa_to_ru.py
@@ -4,6 +4,6 @@ from __future__ import annotations
 from . import rule_engine
 
 
-def convert(ipa_text: str, compiled_rules: list[tuple[str, str]]) -> str:
+def convert(ipa_text: str, replacement_pairs: list[tuple[str, str]]) -> str:
     """Передает готовый массив правил в рантайм-движок."""
-    return rule_engine.process_text(ipa_text, compiled_rules)
+    return rule_engine.process_text(ipa_text, replacement_pairs)
